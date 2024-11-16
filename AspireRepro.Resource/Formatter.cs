82b45510291fb025a4dcce1c059ad34d1aed4e30
Mike Henry <82587+mhenry07@users.noreply.github.com>
2024-11-16 15:38:08 -0800

    Adjust format to generate variable length lines

--- AspireRepro.Resource/Formatter.cs
+++ AspireRepro.Resource/Formatter.cs
@@ -9,17 +9,17 @@ public static class Formatter
     private const int TicksPerMs = 10_000;
     private const int TicksPerSec = 1_000 * TicksPerMs;
 
-    // $"abc,{row},def,{GetTime1(row)},ghi,{row % 1_000},jkl,{GetTime2(row)},mno"
+    // $"abc,{row},def,{new string('a', row % 13 + 1)},ghi,{GetTime1(row)},jkl,{GetTime2(row)},mno"
     public static int Format(Span<byte> buffer, long row, bool eol)
     {
         var written = Encoding.UTF8.GetBytes("abc,", buffer);
         Utf8Formatter.TryFormat(row, buffer[written..], out var bytes);
         written += bytes;
         written += Encoding.UTF8.GetBytes(",def,", buffer[written..]);
-        Utf8Formatter.TryFormat(GetTime1(row), buffer[written..], out bytes);
-        written += bytes;
+        for (var i = 1; i <= row % 13; i++)
+            buffer[written++] = (byte)'a';
         written += Encoding.UTF8.GetBytes(",ghi,", buffer[written..]);
-        Utf8Formatter.TryFormat(row % 1_000, buffer[written..], out bytes);
+        Utf8Formatter.TryFormat(GetTime1(row), buffer[written..], out bytes);
         written += bytes;
         written += Encoding.UTF8.GetBytes(",jkl,", buffer[written..]);
         Utf8Formatter.TryFormat(GetTime2(row), buffer[written..], out bytes);
