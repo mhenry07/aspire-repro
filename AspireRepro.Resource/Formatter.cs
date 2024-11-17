47029e6635f67c5f5ce7967bcab9df97f831e105
Mike Henry <82587+mhenry07@users.noreply.github.com>
2024-11-16 18:38:29 -0800

    Adjust the formatter

--- AspireRepro.Resource/Formatter.cs
+++ AspireRepro.Resource/Formatter.cs
@@ -9,15 +9,16 @@ public static class Formatter
     private const int TicksPerMs = 10_000;
     private const int TicksPerSec = 1_000 * TicksPerMs;
 
-    // $"abc,{row},def,{new string('a', row % 13 + 1)},ghi,{GetTime1(row)},jkl,{GetTime2(row)},mno"
+    // $"abc,{row},def,{new string((char)(row % 26 + (byte)'A'), row % 13 + 1)},ghi,{GetTime1(row)},jkl,{GetTime2(row)},mno"
     public static int Format(Span<byte> buffer, long row, bool eol)
     {
+        var c = (byte)(row % 26 + (byte)'A');
         var written = Encoding.UTF8.GetBytes("abc,", buffer);
         Utf8Formatter.TryFormat(row, buffer[written..], out var bytes);
         written += bytes;
         written += Encoding.UTF8.GetBytes(",def,", buffer[written..]);
         for (var i = 0; i <= row % 13; i++)
-            buffer[written++] = (byte)'a';
+            buffer[written++] = c;
         written += Encoding.UTF8.GetBytes(",ghi,", buffer[written..]);
         Utf8Formatter.TryFormat(GetTime1(row), buffer[written..], out bytes);
         written += bytes;
